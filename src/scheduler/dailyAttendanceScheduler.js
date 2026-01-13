0eb98f3964ff94a43ede63bec62a52d7adb29efb
Misdar <misdarmanto1922@gmail.com>
2026-01-13 22:25:10 +0700

    feat: add new attendance category

--- src/scheduler/dailyAttendanceScheduler.js
+++ src/scheduler/dailyAttendanceScheduler.js
@@ -1,29 +1,21 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-// // setiap jam 00:00
+const node_cron_1 = __importDefault(require("node-cron"));
+const dailyAttendanceService_1 = require("../services/dailyAttendanceService");
+const logs_1 = __importDefault(require("../logs"));
+// // setiap 1 menit
 // export const startDailyAttendanceScheduler = (): void => {
-//   cron.schedule('0 0 * * *', async () => {
+//   cron.schedule('* * * * *', async () => {
 //     logger.info('[CRON] Running daily attendance recap...')
 //     await RecapDailyAttendanceService()
 //   })
 // }
-// cron.schedule(
-//   '0 0 * * *',
-//   async () => {
-//     logger.info('[CRON] Running daily attendance recap (WIB)...')
-//     await RecapDailyAttendanceService()
-//   },
-//   {
-//     timezone: 'Asia/Jakarta'
-//   }
-// )
-// cron.schedule(
-//   '* * * * *',
-//   async () => {
-//     logger.info('[CRON] Running daily attendance recap (WIB)...')
-//     await RecapDailyAttendanceService()
-//   },
-//   {
-//     timezone: 'Asia/Jakarta'
-//   }
-// )
+node_cron_1.default.schedule('0 0 * * *', async () => {
+    logs_1.default.info('[CRON] Running daily attendance recap (WIB)...');
+    await (0, dailyAttendanceService_1.RecapDailyAttendanceService)();
+}, {
+    timezone: 'Asia/Jakarta'
+});
